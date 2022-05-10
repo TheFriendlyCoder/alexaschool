64d08663dcad35e47eb683266a290d95c9f1aa34
Kevin Phillips <thefriendlycoder@gmail.com>
2022-05-10 14:33:17 -0300

    Added basic web scraper support

--- alexaschool.go
+++ alexaschool.go
@@ -2,24 +2,75 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 
 	"github.com/arienmalec/alexa-go"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/thefriendlycoder/schoolscraper"
 )
 
 type Connection struct{}
 
+// Helper function that generates an Alexa response describing a school
+// closure based on a specific school identified by the name and district
+func checkClosureStatus(districtName string, schoolName string) alexa.Response {
+	// Load raw HTTML data from the school website
+	resp, err := http.Get(schoolscraper.ScheduleURL)
+	if err != nil {
+		return alexa.NewSimpleResponse("Failed", "Failed to load school data from URL")
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return alexa.NewSimpleResponse("Failed", "Failed to read school data from URL")
+	}
+
+	// Scrape the HTML content to extract school property data
+	school, err := schoolscraper.GetSchoolStatus(string(body), districtName, schoolName)
+	if err != nil {
+		return alexa.NewSimpleResponse("Failed", "Failed to parse school data from URL")
+	}
+
+	// Generate an appropriate Alexa response depending on the school status
+	var message string
+	if school.IsOpen() {
+		message = "School is open"
+	} else {
+		message = "School is closed"
+	}
+
+	if school.AllBusesOnTime() {
+		message += " and all buses are running on time"
+	} else {
+		message += " and there are some late buses"
+	}
+	return alexa.NewSimpleResponse("School Status", message)
+}
+
+// Callback for handling Alexa requests triggered by "intents" defined
+// by the Alexa app interface. See the JSON files under the
+// ./skill-package/interactionModels folder for details
 func (connection Connection) IntentDispatcher(ctx context.Context, request alexa.Request) (alexa.Response, error) {
+	district := "FREDERICTON"
+	school := "École Sainte-Anne"
 	var response alexa.Response
 	switch request.Body.Intent.Name {
-	case "HelloWorldIntent":
-		response = alexa.NewSimpleResponse("Hi there from Kevin", "It is a wonderful day in the neighborhood")
+	case "SchoolOpenIntent":
+		response = checkClosureStatus(district, school)
+	case "":
+		// This intent is triggered when the user "opens" the app without
+		// providing an actual intent. In this case we assume they want
+		// to check the closure status
+		response = checkClosureStatus(district, school)
 	default:
-		response = alexa.NewSimpleResponse("Unknown Request", "The intent was unrecognized")
+		msg := fmt.Sprintf("Unrecognized intent %v", request.Body.Intent.Name)
+		response = alexa.NewSimpleResponse("Unknown Request", msg)
 	}
 	return response, nil
 }
 
+// Entrypoint function
 func main() {
 	connection := Connection{}
 	lambda.Start(connection.IntentDispatcher)
--- go.mod
+++ go.mod
@@ -4,4 +4,9 @@ go 1.18
 
 require github.com/aws/aws-lambda-go v1.31.1
 
-require github.com/arienmalec/alexa-go v0.0.0-20181025212142-975687393e90
+require (
+	github.com/arienmalec/alexa-go v0.0.0-20181025212142-975687393e90
+	github.com/thefriendlycoder/schoolscraper v0.0.5-0.20220510193328-4e8ffccb9994
+)
+
+require golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect

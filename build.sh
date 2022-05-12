e39d3093bbf6b2c051fd3bfd40a535245cb93bbd
Kevin Phillips <thefriendlycoder@gmail.com>
2022-05-12 19:02:20 -0300

    polished build script slightly

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 GOARCH=amd64 GOOS=linux go build -o alexaschool
 [ -f alexaschool.zip ] && rm alexaschool.zip
 zip alexaschool.zip alexaschool
-aws lambda update-function-code --function-name GoSchool --zip-file fileb://alexaschool.zip
+aws lambda update-function-code --function-name GoSchool --zip-file fileb://alexaschool.zip > /dev/null
 ask deploy
-ask smapi get-skill-status -s amzn1.ask.skill.5cb6ba49-8028-4dc8-87ac-ba0e3ba100a3
+ask smapi get-skill-status -s amzn1.ask.skill.5cb6ba49-8028-4dc8-87ac-ba0e3ba100a3 | jq ".manifest.lastUpdateRequest.status"
